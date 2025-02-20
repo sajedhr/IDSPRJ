10f75b210287c460d389fba9a33773ecdee017fa
sajedhr <sajedharirii2005@gmail.com>
2025-02-20 03:38:19 +0200

    Updated Post.cs and appsettings.json

--- sajedproject/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ sajedproject/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de3307fdfe3598bfb5bd1ee7f9d8e98b010be64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e71e4979d4ddc56deaa64c12dac8b18958e8c21")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
