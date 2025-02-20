47f96738968aac78b1cd233c24693a44524b49b4
sajedhr <sajedharirii2005@gmail.com>
2025-02-20 03:45:29 +0200

    Saved local changes before merge

--- sajedproject/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ sajedproject/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0de3307fdfe3598bfb5bd1ee7f9d8e98b010be64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2184661ec586332276c7181d81da0a6b9860025b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
